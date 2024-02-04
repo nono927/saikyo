01b1daaa138886b4dab0f308cea56144fdb05bf0
nono927 <naokiono927@gmail.com>
2024-02-04 14:45:56 +0900

    rename

--- src/number_theory/sieve_eratosthenes.hpp
+++ src/number_theory/sieve_eratosthenes.hpp
@@ -2,7 +2,7 @@
 
 // 0以上n未満の整数に対して，素数かどうかを判定する．
 // 戻り値のi番目の要素は，iが素数の時に限りtrueである．
-std::vector<bool> sieve_prime(int n) {
+std::vector<bool> sieve_eratosthenes(int n) {
     std::vector<bool> is_primes(n, true);
     is_primes[0] = false;
     is_primes[1] = false;
--- test/number_theory/sieve_eratosthenes_test.cpp
+++ test/number_theory/sieve_eratosthenes_test.cpp
@@ -1,9 +1,9 @@
-#include "number_theory/sieve.hpp"
+#include "number_theory/sieve_eratosthenes.hpp"
 
 #include <gtest/gtest.h>
 
 TEST(SieveTest, Sieve) {
-    std::vector<bool> actual = sieve_prime(8);
+    std::vector<bool> actual = sieve_eratosthenes(8);
     std::vector<bool> expect = {false, false, true,  true,
                                 false, true,  false, true};
     EXPECT_EQ(actual, expect);
