96cffb9d3562fcd663892facbb69d9bf74a10fb5
nono927 <naokiono927@gmail.com>
2024-02-04 17:25:32 +0900

    graph

--- src/graph/graph.hpp
+++ src/graph/graph.hpp
@@ -3,31 +3,46 @@
 #include <cassert>
 #include <vector>
 
+// 重みなしグラフを表すクラス
 struct Graph {
     using node_t = int;
 
     std::vector<std::vector<node_t> > _neighbors;
 
+    // 頂点数nの、辺を持たないグラフを生成する。
     Graph(int n) : _neighbors(std::vector<std::vector<node_t> >(n)) {}
 
-    void check_id_range(node_t v) {
-        assert(v >= 0 && v < static_cast<node_t>(_neighbors.size()));
-    }
-
+    // 有向辺 (u, v) を追加する。
     void add_edge(node_t u, node_t v) {
         check_id_range(v);
         _neighbors[u].push_back(v);
     }
 
+    // 頂点数を返す。
     int size() const { return static_cast<int>(_neighbors.size()); }
 
+    // 頂点vの出次数を返す。
+    int deg(node_t v) const {
+        check_id_range(v);
+        return static_cast<int>(_neighbors[v].size());
+    }
+
+    // 頂点vに隣接する頂点の列を返す。
     const std::vector<node_t>& neighbors(node_t v) const {
         return _neighbors[v];
     }
+
+   private:
+    void check_id_range(node_t v) const {
+        assert(v >= 0 && v < static_cast<node_t>(_neighbors.size()));
+    }
 };
 
-Graph make_directed_graph(int num_nodes, std::vector<Graph::node_t>& xs,
-                          std::vector<Graph::node_t>& ys, int indexed_by = 1) {
+// 頂点数n、有向辺 (xs[i], ys[i]) を持つグラフを生成する。
+Graph make_directed_graph(const int num_nodes,
+                          const std::vector<Graph::node_t>& xs,
+                          const std::vector<Graph::node_t>& ys,
+                          const int indexed_by = 1) {
     assert(xs.size() == ys.size());
     assert(indexed_by >= 0);
 
@@ -44,9 +59,11 @@ Graph make_directed_graph(int num_nodes, std::vector<Graph::node_t>& xs,
     return graph;
 }
 
-Graph make_undirected_graph(int num_nodes, std::vector<Graph::node_t>& xs,
-                            std::vector<Graph::node_t>& ys,
-                            int indexed_by = 1) {
+// 頂点数n、無向辺 (xs[i], ys[i]) を持つグラフを生成する。
+Graph make_undirected_graph(const int num_nodes,
+                            const std::vector<Graph::node_t>& xs,
+                            const std::vector<Graph::node_t>& ys,
+                            const int indexed_by = 1) {
     assert(xs.size() == ys.size());
     assert(indexed_by >= 0);
 
--- src/graph/weighted_graph.hpp
+++ src/graph/weighted_graph.hpp
@@ -4,6 +4,7 @@
 #include <utility>
 #include <vector>
 
+// 重みつきグラフを表すクラス
 struct WeightedGraph {
     using node_t = int;
     using weight_t = long long;
@@ -11,29 +12,40 @@ struct WeightedGraph {
 
     std::vector<std::vector<edge_t> > _neighbors;
 
+    // 頂点数nの、辺を持たないグラフを生成する。
     WeightedGraph(int n) : _neighbors(std::vector<std::vector<edge_t> >(n)) {}
 
-    void check_id_range(node_t v) {
-        assert(v >= 0 && v < static_cast<node_t>(_neighbors.size()));
-    }
-
+    // コストcの有向辺 (u, v) を追加する。
     void add_edge(node_t u, node_t v, weight_t c) {
         check_id_range(v);
         _neighbors[u].emplace_back(v, c);
     }
 
+    // 頂点数を返す。
     int size() const { return static_cast<int>(_neighbors.size()); }
 
+    // 頂点vの出次数を返す。
+    int deg(node_t v) const {
+        check_id_range(v);
+        return static_cast<int>(_neighbors[v].size());
+    }
+
+    // 頂点vから出る辺の集合を、(行き先の頂点, コスト) のタプル列として返す。
     const std::vector<edge_t>& neighbors(node_t v) const {
         return _neighbors[v];
     }
+
+   private:
+    void check_id_range(node_t v) const {
+        assert(v >= 0 && v < static_cast<node_t>(_neighbors.size()));
+    }
 };
 
-WeightedGraph make_directed_wgraph(int num_nodes,
-                                   std::vector<WeightedGraph::node_t>& xs,
-                                   std::vector<WeightedGraph::node_t>& ys,
-                                   std::vector<WeightedGraph::weight_t>& ws,
-                                   int indexed_by = 1) {
+// 頂点数n、重み ws[i] の有向辺 (xs[i], ys[i]) を持つグラフを生成する。
+WeightedGraph make_directed_wgraph(
+    const int num_nodes, const std::vector<WeightedGraph::node_t>& xs,
+    const std::vector<WeightedGraph::node_t>& ys,
+    const std::vector<WeightedGraph::weight_t>& ws, const int indexed_by = 1) {
     assert(xs.size() == ys.size());
     assert(indexed_by >= 0);
 
@@ -49,11 +61,11 @@ WeightedGraph make_directed_wgraph(int num_nodes,
     return graph;
 }
 
-WeightedGraph make_undirected_wgraph(int num_nodes,
-                                     std::vector<WeightedGraph::node_t>& xs,
-                                     std::vector<WeightedGraph::node_t>& ys,
-                                     std::vector<WeightedGraph::weight_t>& ws,
-                                     int indexed_by = 1) {
+// 頂点数n、重み ws[i] の無向辺 (xs[i], ys[i]) を持つグラフを生成する。
+WeightedGraph make_undirected_wgraph(
+    const int num_nodes, const std::vector<WeightedGraph::node_t>& xs,
+    const std::vector<WeightedGraph::node_t>& ys,
+    const std::vector<WeightedGraph::weight_t>& ws, const int indexed_by = 1) {
     assert(xs.size() == ys.size());
     assert(indexed_by >= 0);
 
