963a4ea1d4d18dc9c5e40e67e780137e7eb1f6b6
nono927 <naokiono927@gmail.com>
2024-02-25 14:26:30 +0900

    refactor dump

--- utils/templates/local_debug.hpp
+++ utils/templates/local_debug.hpp
@@ -1,87 +1,129 @@
 #pragma once
 
+#include <atcoder/modint>
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <tuple>
 #include <type_traits>
-
-#define ASSERT(x) assert(x)
+#include <vector>
 
 namespace local_debug {
 
-#define DEFINE_HAS_MEMBER(member_name)                      \
-    template <class T>                                      \
-    struct has_member_##member_name {                       \
-       private:                                             \
-        template <class U>                                  \
-        static constexpr auto has_member(U&& x)             \
-            -> decltype(x.member_name, std::true_type());   \
-        static constexpr std::false_type has_member(...);   \
-                                                            \
-       public:                                              \
-        static constexpr bool value =                       \
-            decltype(has_member(std::declval<T>()))::value; \
-    }
+const char* ESC_DEFAULT = "\e[0m";
+const char* ESC_BOLD = "\e[1m";
+const char* ESC_CYAN = "\e[36m";
 
-#define DEFINE_HAS_FUNC(func_name)                        \
-    template <class T>                                    \
-    struct has_func_##func_name {                         \
-       private:                                           \
-        template <class U>                                \
-        static constexpr auto has_func(U&& x)             \
-            -> decltype(x.func_name(), std::true_type()); \
-        static constexpr std::false_type has_func(...);   \
-                                                          \
-       public:                                            \
-        static constexpr bool value =                     \
-            decltype(has_func(std::declval<T>()))::value; \
-    }
-
-DEFINE_HAS_FUNC(begin);
-DEFINE_HAS_FUNC(val);
-DEFINE_HAS_FUNC(dump);
-DEFINE_HAS_MEMBER(first);
-DEFINE_HAS_MEMBER(second);
 template <class T>
-inline constexpr bool is_pair_v = (has_member_first<T>::value ||
-                                   has_member_second<T>::value);
-#undef DEFINE_HAS_MEMBER
-#undef DEFINE_HAS_FUNC
+concept Tuple = requires(T a) { std::tuple_size<T>::value; };
+
+template <class T>
+concept Container = requires(T a) {
+    a.begin();
+    a.end();
+} && !std::same_as<T, std::string>;
+
+template <Tuple T, std::size_t N>
+void dump_tuple(T x, bool not_first);
+template <Container T>
+void dump_container(const T container, const char lbrace, const char rbrace);
+
+// =================
+// dump functions
+// =================
 
 template <class T>
 void dump(T x) {
-    if constexpr (std::is_arithmetic_v<T>) {
-        std::cerr << x;
-    } else if constexpr (std::is_same_v<T, std::string>) {
-        std::cerr << "\"" << x << "\"";
-    } else if constexpr (is_pair_v<T>) {
-        std::cerr << "(";
-        dump(x.first);
-        std::cerr << ", ";
-        dump(x.second);
-        std::cerr << ")";
-    } else if constexpr (has_func_dump<T>::value) {
-        x.dump();
-    } else if constexpr (has_func_val<T>::value) {
-        dump(x.val());
-    } else if constexpr (has_func_begin<T>::value) {
-        std::cerr << "{";
-        for (auto&& e : x) {
-            dump(e);
-            std::cerr << ", ";
-        }
-        if (x.size() > 0) {
-            std::cerr << "\e[2D";
-        }
-        std::cerr << "}";
+    static_assert([] { return false; }());
+}
+
+void dump(bool x) { std::cerr << (x ? "true" : "false"); }
+
+template <std::integral T>
+void dump(T x) {
+    std::cerr << x;
+}
+
+template <std::floating_point T>
+void dump(T x) {
+    std::cerr << x;
+}
+
+void dump(std::string x) { std::cerr << '"' << x << '"'; }
+
+void dump(std::vector<bool> x) {
+    bool not_first = false;
+    std::cerr << '[';
+    for (auto&& e : x) {
+        if (not_first) std::cerr << ", ";
+        dump((bool)e);
+        not_first = true;
+    }
+    std::cerr << ']';
+}
+
+template <class T>
+void dump(std::vector<T> x) {
+    dump_container(x, '[', ']');
+}
+
+template <Tuple T>
+void dump(T x) {
+    std::cerr << "(";
+    dump_tuple<T, 0>(x, false);
+    std::cerr << ")";
+}
+
+template <class T>
+    requires std::derived_from<T, atcoder::internal::modint_base>
+void dump(T x) {
+    dump(x.val());
+}
+
+template <Container T>
+void dump(T x) {
+    dump_container(x, '{', '}');
+}
+
+// ===================
+// helper functions
+// ===================
+
+template <Tuple T, std::size_t N>
+void dump_tuple(T x, bool not_first) {
+    if constexpr (N >= std::tuple_size<T>::value) {
+        return;
     } else {
-        static_assert([] { return false; }());
+        if (not_first) std::cerr << ", ";
+        dump(std::get<N>(x));
+        dump_tuple<T, N + 1>(x, true);
     }
 }
 
-#define DUMP_HEADER(x)                                                    \
-    std::cerr << "\e[1m\e[36m[DUMP: line " << __LINE__ << "]\e[0m " << #x \
-              << " : "
+template <Container T>
+void dump_container(const T container, const char lbrace, const char rbrace) {
+    bool not_first = false;
+    std::cerr << lbrace;
+    for (auto&& x : container) {
+        if (not_first) std::cerr << ", ";
+        dump(x);
+        not_first = true;
+    }
+    std::cerr << rbrace;
+}
+
+}  // namespace local_debug
+
+// =========
+// macros
+// =========
+
+#define ASSERT(x) assert(x)
+
+#define DUMP_HEADER(x)                                          \
+    std::cerr << local_debug::ESC_BOLD << local_debug::ESC_CYAN \
+              << "[DUMP: line " << __LINE__ << "] "             \
+              << local_debug::ESC_DEFAULT << #x << " : "
 
 #define DUMP(x)                 \
     do {                        \
@@ -89,5 +131,3 @@ void dump(T x) {
         local_debug::dump(x);   \
         std::cerr << std::endl; \
     } while (false)
-
-}  // namespace local_debug
